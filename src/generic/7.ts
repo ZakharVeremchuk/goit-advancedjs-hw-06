0bc812287ea0bface0e207d281ffa7d45be36ba1
Zakhar Veremchuk <zaharverem@gmail.com>
2025-06-21 20:59:20 +0300

    Finished tasks

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -9,8 +9,10 @@ export enum UserRole {
   guest = 'guest',
 }
 
+type RoleType = Record<UserRole, string>
+
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription: RoleType = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
